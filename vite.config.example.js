d3d712b025ee65df61e08dd32b02208661378dbf
hmh_6 <www.hussamcr7@gmail.com>
2022-07-11 16:09:15 +0300

    up

--- vite.config.example.js
+++ vite.config.example.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite'
+
 
 export default defineConfig({
     server:{
@@ -8,6 +10,13 @@ export default defineConfig({
         port:5000 // SAME PORT WITH LARAVEL //
     },
     plugins: [
+        AutoImport({
+            imports: ['vue'],
+            dirs: [
+                './resources/js/Layouts',
+            ],
+            vueTemplate: true,
+        }),
         laravel({
             input: 'resources/js/app.js',
         }),
